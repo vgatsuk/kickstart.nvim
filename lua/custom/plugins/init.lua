907b1038a294c8d77fda4b04ea937edf93eba820
Valentyn Gatsuk <valentyn@lindenlab.com>
2025-03-20 17:31:12 +0000

    Adding better gitsigns keymaps

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -11,7 +11,16 @@ vim.opt.foldenable = false
 vim.g.termguicolors = true
 -- vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<cr>', { silent = true, noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>e', ':Neotree action=focus source=filesystem position=left toggle=true<cr>', { silent = true, noremap = true })
--- vim.api.nvim_set_keymap('n', '<Leader>b', ':Neotree action=focus source=buffers position=left toggle=true<cr>', { silent = true, noremap = true })
+-- Working with gitsigns changes
+-- -- moving between git changes
+vim.api.nvim_set_keymap('n', ']g', ':Gitsigns nav_hunk next<cr>', { silent = true, noremap = true, desc = '[G]itsigns nav_hunk next' })
+vim.api.nvim_set_keymap('n', '[g', ':Gitsigns nav_hunk prev<cr>', { silent = true, noremap = true, desc = '[G]itsigns nav_hunk prev' })
+-- -- staging/reverting git changes
+vim.api.nvim_set_keymap('n', '<leader>gs', ':Gitsigns stage_hunk<cr>', { silent = true, noremap = true, desc = '[G]itsigns [s]tage_hunk' })
+vim.api.nvim_set_keymap('n', '<leader>gr', ':Gitsigns reset_hunk<cr>', { silent = true, noremap = true, desc = '[G]itsigns [r]eset_hunk' })
+vim.api.nvim_set_keymap('n', '<leader>gp', ':Gitsigns preview_hunk_inline<cr>', { silent = true, noremap = true, desc = '[G]itsigns [p]review_hunk_inline' })
+vim.api.nvim_set_keymap('n', '<leader>gb', ':Gitsigns blame<cr>', { silent = true, noremap = true, desc = '[G]itsigns [b]lame' })
+vim.api.nvim_set_keymap('n', '<leader>gq', ':Gitsigns setqflist target=all<cr>', { silent = true, noremap = true, desc = '[G]itsigns set[q]flist target=all' })
 -- vim.api.nvim_set_keymap('n', '<Leader>g', ':Neotree action=focus source=github position=left toggle=true<cr>', { silent = true, noremap = true })
 
 vim.wo.relativenumber = true
