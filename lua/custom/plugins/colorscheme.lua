5b529f2a6700d70dbb484d41978ff39fe47d8f4c
Valentyn Gatsuk <vgatsuk@gmail.com>
2025-03-26 13:55:32 -0700

    Fixing colorschemes some

---
+++ lua/custom/plugins/colorscheme.lua
@@ -0,0 +1,105 @@
+return {
+  --{ -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  -- 'folke/tokyonight.nvim',
+  -- priority = 1000, -- Make sure to load this before all the other start plugins.
+  -- config = function()
+  --   ---@diagnostic disable-next-line: missing-fields
+  --   require('tokyonight').setup {
+  --     styles = {
+  --       comments = { italic = false }, -- Disable italics in comments
+  --     },
+  --   }
+  --
+  --   -- Load the colorscheme here.
+  --   -- Like many other themes, this one has different styles, and you could load
+  --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --   -- vim.cmd.colorscheme 'tokyonight-night'
+  -- end,
+  -- },
+  -- catppuccin
+  {
+    'catppuccin/nvim',
+    priority = 1000,
+    name = 'catppuccin',
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'mocha', -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = 'latte',
+          dark = 'mocha',
+        },
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = {}, -- Change the style of comments
+          conditionals = { 'italic' },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+      }
+      vim.cmd.colorscheme 'catppuccin-mocha'
+    end,
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        fzf = true,
+        grug_far = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { 'undercurl' },
+            hints = { 'undercurl' },
+            warnings = { 'undercurl' },
+            information = { 'undercurl' },
+          },
+        },
+        navic = { enabled = true, custom_bg = 'lualine' },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        snacks = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+    specs = {
+      {
+        'akinsho/bufferline.nvim',
+        optional = true,
+        opts = function(_, opts)
+          if (vim.g.colors_name or ''):find 'catppuccin' then
+            opts.highlights = require('catppuccin.groups.integrations.bufferline').get()
+          end
+        end,
+      },
+    },
+  },
+}
